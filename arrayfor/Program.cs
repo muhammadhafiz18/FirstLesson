40e88c1bedbb576806a443364fe4e9fff6c01fd6
zaxro <ergashevazahro001@gmail.com>
2025-02-17 20:30:26 +0500

    sorting algorithm

---
+++ arrayfor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 6, 5, 1, 7, 8, 3, 4 };
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+
+                if (arr[j] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            Console.writeLine(arr[i]);
+        }
+    }
+}
