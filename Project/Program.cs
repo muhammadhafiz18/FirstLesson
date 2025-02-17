fc5ac582ecb590a6e94c9fc1ee470f62f6a4785b
Xojimurodov <xojimurodovdiyorbek2@gmail.com>
2025-02-17 19:02:13 +0500

    Array sort

---
+++ Project/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = [3,5,1,8,2];
+int temp = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    for(int a = 0; a < array.Length; a++)
+    {
+        if(array[i] < array[a])
+        {
+            temp = array[i];
+            array[i] = array[a];
+            array[a] = temp;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", array));
