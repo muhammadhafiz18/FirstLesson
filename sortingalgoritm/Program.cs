ad0f0354fca58b124a24c02f9b5fb0af9aa335fb
Firdavs <firdavsmukhsimov@gmail.com>
2025-02-17 19:32:28 +0500

    added sorting algorithm

---
+++ sortingalgoritm/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+
+    int[] arr = [5, 8, 9, 1, 10];
+
+    int temp = 0;
+
+
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        for(int p = 0 ; p < arr.Length; p++)
+        {
+            if(arr[i] < arr[p])
+            {
+                temp = arr[i];
+                arr[i] = arr[p];
+                arr[p] = temp;
+
+            }
+        }
+    }
+
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i] + " ");
+    }
+    }
+}
